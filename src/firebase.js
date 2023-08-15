61e050fb1ce9e4e5322c702c6df1c5319c98bb11
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2023-08-15 17:06:40 +0530

    created react-app installed firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkuPjIEB_X0YNr08IOA9vk_Wm2VG73h00",
+  authDomain: "mobilesignin-391ca.firebaseapp.com",
+  projectId: "mobilesignin-391ca",
+  storageBucket: "mobilesignin-391ca.appspot.com",
+  messagingSenderId: "384495131938",
+  appId: "1:384495131938:web:b596e44875c394b625997b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
